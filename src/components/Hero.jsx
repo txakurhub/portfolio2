51f3225c546728b272a4abcc763d0a1c5eee9ff6
Leandro Pereyra <me.leandrop@gmail.com>
2023-02-07 09:26:34 -0300

    sections

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+const Hero = () => {
+  return (
+    <section id="intro" class="main style1 dark fullscreen">
+      <div class="content">
+        <header>
+          <h2>Hey.</h2>
+        </header>
+        <p>Let's transform your ideas into an effective web experience</p>
+        <footer>
+          <a href="#about" class="button style2 down">
+            More
+          </a>
+        </footer>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
